b28ec7a83d0752ba9cdcab4dfeb23661afb2076f
alyssa horrocks <alyssa.horrocks2@gmail.com>
2015-10-16 10:43:33 -0700

    Add posts#edit and posts#update

---
+++ spec/features/edit_post_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'the edit a post process' do
+  it 'edits a post' do
+    post = Post.create(title: 'Test Post', content: 'This is a test post.')
+    visit edit_post_path(post)
+    fill_in 'Title', :with => 'Testing Testing'
+    fill_in 'Content', :with => 'I am testing fa la la la la.'
+    click_on 'Submit'
+    expect(page).to have_content 'Testing Testing'
+  end
+
+  it 'shows an error when nothing is filled in' do
+    post = Post.create(title: 'Test Post', content: 'This is a test post.')
+    visit edit_post_path(post)
+    fill_in 'Title', :with => ''
+    fill_in 'Content', :with => ''
+    click_on 'Submit'
+    expect(page).to have_content 'errors'
+  end
+
+end
