291271af4749a18425aeaabafec8e7b270f1dc7d
alyssa horrocks <alyssa.horrocks2@gmail.com>
2015-10-16 10:54:28 -0700

    Add posts#destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :presence => true
   has_and_belongs_to_many :tags
+
+  before_save(:title_case)
+
+  private
+    def title_case
+      title = self.title.downcase
+      words = title.split(' ')
+      words.map do |word|
+        word.capitalize!
+      end
+      self.title = words.join(' ')
+    end
 end
---
+++ spec/features/destroy_post_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'the process to destroy a post' do
+  it 'delete a post' do
+    post = Post.create(title: "Post to Destroy", content: "This will be deleted")
+    visit post_path(post)
+    click_on 'Delete Post'
+    expect(page).to_not have_content 'Post to Destroy'
+  end
+end
