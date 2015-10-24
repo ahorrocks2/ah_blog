b8120cd4ac96cad4b9f7e76c6d15bf6033ac875f
Alyssa Horrocks <am.rocks@outlook.com>
2015-10-24 15:02:12 -0400

    Add comment#destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
     if @comment.save
       redirect_to post_path(@post)
     else
@@ -30,10 +31,14 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
 private
   def comment_params
-    params.require(:comment).permit(:name, :content)
+    params.require(:comment).permit(:name, :content, :user_id)
   end
 end
